ea3af221a3463eda282a9a6afd560b4b156dd0c8
samuel14luis <luismendoza.samuel@gmail.com>
2023-12-03 10:19:36 -0500

    feat: vowels counter

--- src/exercises/vowels.rs
+++ src/exercises/vowels.rs
@@ -8,7 +8,32 @@ pub fn get_title() -> &'static str {
 }
 
 pub fn run() {
-    println!("TODO: Contar vocales");
+    println!("Ingresa el texto a evaluar:");
+
+    let mut n = String::new();
+
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Falla al leer la linea");
+
+    let result = vowels_count(n.trim());
+
+    println!("Hemos encontrado {:?} vocales.", result);
 
     utils::show_press_enter();
+}
+
+fn vowels_count(s: &str) -> usize {
+    let mut total = 0;
+    let vowels = vec![
+        "a","e","i","o","u",
+        "A","E","I","O","U",
+        "á","é","í","ó","ú",
+        "Á","É","Í","Ó","Ú"];
+    for c in s.chars() {
+        if vowels.contains(&c.to_string().as_str()) {
+            total += 1;
+        }
+    }
+    total
 }
