5e5866457f73feed67eff91563ca0f8df06c8d8d
samuel14luis <luismendoza.samuel@gmail.com>
2023-12-03 23:48:38 -0500

    feat: binary search

--- src/exercises/binary_search.rs
+++ src/exercises/binary_search.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::time::{Instant, Duration};
 
 #[path = "../utils/utils.rs"]
 mod utils;
@@ -8,7 +8,52 @@ pub fn get_title() -> &'static str {
 }
 
 pub fn run() {
-    println!("TODO: Búsqueda binaria");
+    println!("Ingresa el arreglo de cuantos números deseas generar.");
+    let n: usize = utils::read_usize();
 
+    // generar un vector con n números aleatorios
+    let numbers: Vec<i32> = utils::generate_random_i32_vec(n);
+
+    println!("Arreglo generado correctamente:");
+    println!("{:?}", numbers);
+
+    // leer el número a buscar
+    println!("Ingresa el número que deseas buscar en el arreglo generado.");
+    let n: i32 = utils::read_i32();
+
+    let start = Instant::now();
+
+    // hacer una búsqueda binaria
+    let index = binary_search(numbers, n);
+
+    let duration: Duration = start.elapsed();
+
+    if index == -1 {
+        println!("El número {} no se encuentra en el arreglo.", n);
+    } else {
+        println!("El número {} se encuentra en el índice {} del arreglo.", n, index);
+    }
+
+    utils::show_duration(duration);
     utils::show_press_enter();
+}
+
+fn binary_search(arr: Vec<i32>, n: i32) -> i32 {
+    let mut left: usize = 0;
+    let mut right: usize = arr.len() - 1;
+    let mut middle: usize;
+
+    while left <= right {
+        middle = (left + right) / 2;
+
+        if arr[middle] == n {
+            return middle as i32;
+        } else if arr[middle] < n {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+
+    return -1;
 }
