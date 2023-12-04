62318eab19cfbd47a83422e4e2a21bc39b3b4096
samuel14luis <luismendoza.samuel@gmail.com>
2023-12-03 23:14:25 -0500

    feat: utils random numbers & vectors generators

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,4 +1,5 @@
-use std::time::Duration;
+use std::{time::Duration, io};
+use rand::Rng;
 
 pub fn show_press_enter() {
     println!("Presiona ENTER para continuar...");
@@ -26,4 +27,64 @@ pub fn show_duration(duration: Duration) {
     };
 
     println!("{}", message);
+}
+
+pub fn random_i32() -> i32 {
+    let mut rng = rand::thread_rng();
+    return rng.gen::<i32>();
+}
+
+pub fn random_usize() -> usize {
+    let mut rng = rand::thread_rng();
+    return rng.gen::<usize>();
+}
+
+pub fn generate_random_i32_vec(lenght: usize) -> Vec<i32> {
+    let mut vec = vec![];
+
+    for _ in 0..lenght {
+        vec.push(random_i32());
+    }
+
+    return vec;
+}
+
+pub fn generate_random_usize_vec(lenght: usize) -> Vec<usize> {
+    let mut vec: Vec<usize> = vec![];
+
+    for _ in 0..lenght {
+        vec.push(random_usize());
+    }
+
+    return vec;
+}
+
+pub fn read_usize() -> usize {
+    let mut n = String::new();
+
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Falla al leer la linea");
+
+    let n: usize = n
+        .trim()
+        .parse()
+        .expect("Se ingréso un valor no numérico");
+
+    return n;
+}
+
+pub fn read_i32() -> i32 {
+    let mut n = String::new();
+
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Falla al leer la linea");
+
+    let n: i32 = n
+        .trim()
+        .parse()
+        .expect("Se ingréso un valor no numérico");
+
+    return n;
 }
