68a558582d5e3ef01cbbc4789af95e0a1ae39909
samuel14luis <luismendoza.samuel@gmail.com>
2023-12-03 23:14:41 -0500

    feat: linear search

--- src/exercises/linear_search.rs
+++ src/exercises/linear_search.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::time::{Instant, Duration};
 
 #[path = "../utils/utils.rs"]
 mod utils;
@@ -8,7 +9,37 @@ pub fn get_title() -> &'static str {
 }
 
 pub fn run() {
-    println!("TODO: Búsqueda lineal");
+    println!("Ingresa el arreglo de cuantos números deseas generar.");
+    let n: usize = utils::read_usize();
 
+    // generar un vector con n números aleatorios
+    let numbers: Vec<usize> = utils::generate_random_usize_vec(n);
+
+    println!("Arreglo generado correctamente:");
+    println!("{:?}", numbers);
+
+    // leer el número a buscar
+    println!("Ingresa el número que deseas buscar en el arreglo generado.");
+    let n: usize = utils::read_usize();
+
+    let start = Instant::now();
+    // hacer una búsqueda lineal
+    let mut index = -1;
+    for i in 0..numbers.len() {
+        if numbers[i] == n {
+            index = i as i32;
+            break;
+        }
+    }
+
+    let duration: Duration = start.elapsed();
+
+    if index == -1 {
+        println!("El número {} no se encuentra en el arreglo.", n);
+    } else {
+        println!("El número {} se encuentra en el índice {} del arreglo.", n, index);
+    }
+
+    utils::show_duration(duration);
     utils::show_press_enter();
 }
